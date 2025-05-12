e021636a9166a708203675708ec45aad8698bd67
LocalTrickster <eduardoacostacrick58@hotmail.com>
2025-05-12 02:22:01 -0300

    Tryinc once again

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 import HelloWorldScene from "./scenes/HelloWorldScene.js";
+import WinCondition from "./scenes/WinCondition.js";
+import LosingCondition from "./scenes/LosingCondition.js";
+import NegativeScoreScene from "./scenes/NegativeScoreScene.js"; 
 
-// Create a new Phaser config object
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   scale: {
     mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
+    autoCenter: Phaser.Scale.CENTER_BOTH, 
     min: {
       width: 800,
       height: 600,
@@ -20,15 +22,11 @@ const config = {
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 200 },
-      debug: true,
+      gravity: { y: 300 },
+      debug: false,
     },
   },
-  // List of scenes to load
-  // Only the first scene will be shown
-  // Remember to import the scene before adding it to the list
-  scene: [HelloWorldScene],
+  scene: [HelloWorldScene, WinCondition, LosingCondition, NegativeScoreScene],
 };
 
-// Create a new Phaser game instance
-window.game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
